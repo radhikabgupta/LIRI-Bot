35d96047eec92dbfe11473b2f25157c873853e54
realzaidiguy <realzaidiguy@gmail.com>
2016-12-08 18:46:11 -0500

    require twitter spotify and request libraries

--- liri.js
+++ liri.js
@@ -1,13 +1,18 @@
 // require the keys.js file that holds the twitter keys
 var twitterKeysObject = require('./keys.js');
 
+// require twitter, spotify, and request NPM libraries
+var twitter = require('twitter');
+var spotify = require('spotify');
+var request = require('request');
+
 // the data is received as an object, but original data was an object too
 // so peel back one layer and get to the actual keys object
 var twitterKeys = twitterKeysObject.twitterKeys;
 
 // console log to verify
-console.log(twitterKeysObject);
-console.log(twitterKeys);
+// console.log(twitterKeysObject);
+// console.log(twitterKeys);
 
 // possible command line commands include:
 // my-tweets, spotify-this-song, movie-this, do-what-it-says
